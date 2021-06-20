07aeaeb6d3f2777ff1f99ea0c533edc7267a53e0
ivarsaris <ivarsaris@gmail.com>
2021-06-20 18:53:24 +0800

    created base template

--- sure_cues/urls.py
+++ sure_cues/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webpage.urls')),
+    path('about/', include('webpage.urls')),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Andreas',
+        'title': 'news1',
+        'content': 'I have a new website!',
+        'date': '20-05-2021'
+    }, 
+    {
+        'author': 'Andreas',
+        'title': 'news2',
+        'content': 'I have a new home!',
+        'date': '20-06-2021'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Home page</h1>')
+    info = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'webpage/index.html', info)
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'webpage/about.html', {'title': 'About'})
